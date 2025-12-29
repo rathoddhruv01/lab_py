3d5b05ff2cbe5811f61203c593aeb918678de2b9
rathoddhruv01 <dhruvrathod744@gmail.com>
2025-12-29 11:24:23 +0530

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,46 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x="hello world"
+print(x)
+hello world
+x="20"
+print(x)
+20
+x=["apple","banana","cherry"]
+print(x)
+['apple', 'banana', 'cherry']
+x=("apple","banana","cherry")
+print(x)
+('apple', 'banana', 'cherry')
+x=20.5
+print(x)
+20.5
+a=10
+b=15
+c=x+b
+print(c)
+35.5
+c=a-b
+print(c)
+-5
+c=a*b
+print(c)
+150
+
+
+
+
+x=1j
+print(x)
+1j
+1
+print(a<b and b>0)
+True
+ 
+print(a<b and b>16)
+print(a<b and b>16)
+
+
+
+
+
