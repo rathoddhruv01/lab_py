326f1ba5487c336186568e4aa644ec17bbf9eb85
rathoddhruv01 <dhruvrathod744@gmail.com>
2026-01-28 09:08:49 +0530

    Add files via upload

---
+++ program4.py
@@ -0,0 +1,24 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x=5
+y=6
+print(a & b)
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    print(a & b)
+NameError: name 'a' is not defined
+x=5
+y=6
+print(x & y)
+4
+x=9
+y=6
+print(x ^ y)
+15
+x=3
+y=6
+print(x | y)
+7
+x=6
+print(~x)
+-7
