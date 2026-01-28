cf0f413995f4527d4dc6973c7488b9ea8cf96653
rathoddhruv01 <dhruvrathod744@gmail.com>
2026-01-28 07:51:38 +0530

    Add files via upload

---
+++ assigment2.py
@@ -0,0 +1,57 @@
+#Q-1
+for i in range(1, 6):
+ print(i)  
+
+#Q-2
+my_string = "Hello, Python!"
+length = len(my_string)
+print("\n\tString length is : ",length)
+
+#Q-3
+a = 10
+b = 20
+maximum = max(a, b)
+print("\n\tMaximum is:", maximum)
+
+#Q-4
+P = 1000
+R = 5
+T = 2
+T = SI = (P * R * T) / 100
+SI = (P * R * T) / 100
+print("\n\tSimple Interest is:", SI)
+
+#Q-5
+print("\n\twelcom")
+
+#Q-6
+my_string = "Python"
+first_char = my_string[0]
+print("\n\tThe first character is:", first_char)
+
+#Q-7
+last_char = my_string[-1]
+last_char = my_string[-1]
+print("\n\tThe last character is:", last_char)
+
+#Q-8
+num = float(input("Enter a number: "))
+if num > 0:
+   print("\n\tThe number is positive.")
+elif num < 0:
+    print("\n\tThe number is negative.")
+else:
+    print("\n\tThe number is zero.")
+
+#Q-9
+a = 10
+b = 20
+c = 30
+sum_of_numbers = a + b + c
+print("\n\tThe sum is:", sum_of_numbers)
+ 
+#Q-10
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+sum_of_numbers = num1 + num2
+print("\n\tThe sum of the three numbers is:", sum_of_numbers)
