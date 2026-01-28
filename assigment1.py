49db75eba22bdeee20debab757eb18c5ad707f8e
rathoddhruv01 <dhruvrathod744@gmail.com>
2026-01-28 07:49:00 +0530

    Add files via upload

---
+++ assigment1.py
@@ -0,0 +1,68 @@
+#Q-1
+x="hello"
+print(x)
+
+print("\n")
+
+#Q-2
+a="Rajesh\tkumar\nFlat\tNo.101,Sunshine\tApartments\nMG\tRoads,Sector\t15\nRajkot,\nPincode:360004,\nIndia"
+print(a)
+
+print("\n")
+
+#Q-3
+b=150
+c=120.50
+print(b+c)
+print(b-c)
+print(b*c)
+print(b/c)
+
+print("\n")
+
+#Q-4
+pi=3.14
+radius = float(input("\nEnter Radius :"))
+area = pi*radius*radius
+circumference = 2*pi*radius
+print("\n\tArea Of Circle :",area)
+print("\n\tCircumference Of Circle :",circumference)
+
+#Q-5
+p = float(input("\nEnter Principal Amount :"))
+r = float(input("Enter Rate Of Interest :"))
+t = float(input("Enter Time (In Years) :"))
+SI = (p*r*t) /100
+print("\n\tSimple Interest",SI)
+
+#Q-6
+length = float(input("\nEnter Length :"))
+width = float(input("Enter Width :"))
+perimeter = 2*length*width
+print("\n\tPerimeter Of Rectangle",perimeter)
+
+#Q-7
+length = float(input("\nEnter Length :"))
+width = float(input("Enter Width :"))
+area = length*width
+perimeter = 2*(length+width)
+print("\n\tArea Of Rectangle",area)
+print("\n\tPerimeter Of Rectangle",perimeter)
+
+#Q-8
+a = float(input("Enter the length of side a: "))
+b = float(input("Enter the length of side b: "))
+c = float(input("Enter the length of side c: "))
+perimeter = a + b + c
+print("\n\tThe perimeter of the triangle is:", perimeter)
+
+#Q-9
+side = float(input("Enter the length of the side of the square: "))
+area = side * side
+print("\n\tThe area of the square is:", area)
+
+#Q-10
+slide = float(input("Enter the length of the slide of the square: "))
+perimeter = 4 * slide
+print("\n\tThe perimeter of the square is:", perimeter)
+
