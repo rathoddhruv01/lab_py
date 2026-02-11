1bb87ee63b668612ff68aeae7600f58c192cce9d
rathoddhruv01 <dhruvrathod744@gmail.com>
2026-02-11 08:46:34 +0530

    Add files via upload

---
+++ conditional_statment.py
@@ -0,0 +1,27 @@
+"""age = int(input("enter your age:"))
+if age >=18:
+    print("yes you are eligible")
+else:
+    print("you arrer not eligible")"""
+    
+"""marks = int(input("enter your marks :"))
+if marks >= 90:
+    print("you will get A greade")
+elif marks >= 70:
+    print("you will get B greade")
+else:
+    print("you will get C greade")"""
+
+"""age=int(input("enter your age:"))
+if age>=18:
+    print("you are eligible to vote:")
+else:
+    print("you are not eligible to vote:")"""  
+    
+num=int(input("enter a number:"))
+if num>0:
+    print("positive number")
+elif num<0:
+    print("negative number")
+else:
+    print("zero")
