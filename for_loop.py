9adce15fc8c5fa23f9a1914179328de183dbcb99
rathoddhruv01 <dhruvrathod744@gmail.com>
2026-02-18 09:11:40 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,40 @@
+""" print number from 1 to 10 
+for i in range(1,11):
+    print(i)
+    
+print even number from 1 to 20 
+for i in range(1,21):
+  if i%2==0:
+      print(i) 
+      
+for i in range(1,11):
+    if i%2==0:
+        print(i)
+    
+for i in range (1,11):
+    print("5 x",i,"=",5*i) 
+#print chjaracter of string 
+name="atmiya"
+for letter in name:
+    print(letter)
+    
+#sum of number from 1 to 5
+total=0
+for i in range(1,6):
+    total=total+i
+    print("sum is:",total)  
+    
+#print list elements 
+
+number = [10,20,30,40]
+for n in number:
+  print(n)
+    
+#print number 1 to 5
+
+for i in range(1,6):
+    print(i) """
+    
+#print a message 3 times
+ for i in range (3):
+    print("hello")
